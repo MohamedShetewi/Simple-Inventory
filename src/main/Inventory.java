add351125c0f9b1cb9c776467ebc239fba62f223
mohamedshetewi <muhamedshetewi@gmail.com>
2023-03-12 21:56:00 +0200

    Implemented Skeleton for the methods

---
+++ src/main/Inventory.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Inventory {
+    private ArrayList<Item> itemsList;
+
+    public Inventory() {
+        this.itemsList = new ArrayList<>();
+    }
+
+    public void addItem(String name, String description, int quantity) {
+
+    }
+
+    public void removeItem(String id) {
+
+    }
+
+    public void updateItem(String itemId, String name, String description, int quantity) {
+
+    }
+
+    public Item getItem(String id) {
+        return null;
+    }
+
+    public ArrayList<Item> getInventory() {
+        return null;
+    }
+}
---
+++ src/main/Item.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Item {
+    private String id, name, description;
+    private int quantity;
+
+    public Item(String id, String name, String description, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+}
